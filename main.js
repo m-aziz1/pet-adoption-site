1d22c990794181462d88e284cc79e762a4e6b849
m-aziz1 <m.ihsanaziz1@gmail.com>
2024-09-01 17:44:48 -0600

    Fetch pet data:

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 async function start() {
-  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
+  const weatherPromise = await fetch(
+    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+  );
   const weatherData = await weatherPromise.json();
 
   const ourTemperature = weatherData.properties.periods[0].temperature;
   document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
-start()
+start();
+
+async function petsArea() {
+  const petsPromise = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+  );
+  const petsData = await petsPromise.json();
+  petsData.forEach(pet => {
+    console.log(pet.name);
+  });
+}
+
+petsArea();
